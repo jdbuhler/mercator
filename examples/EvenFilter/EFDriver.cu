c3062af6867a6f2aa5b6653f82c4b0cb83cb501e
Jeremy Buhler <jbuhler@cassini.seas.wustl.edu>
2019-10-03 15:25:15 -0500

    remove spurious debug lines

--- examples/EvenFilter/EFDriver.cu
+++ examples/EvenFilter/EFDriver.cu
@@ -16,9 +16,7 @@ using namespace std;
 int main()
 {
   const unsigned int NVALUES = 1000000000; // one BEEEELLION values
-  //const unsigned int NVALUES = 10000000; // one BEEEELLION values
-  //const unsigned int NVALUES = 100000; // one BEEEELLION values
-
+  
   srand(0);
   
   unsigned int *inputValues = new unsigned int [NVALUES];
