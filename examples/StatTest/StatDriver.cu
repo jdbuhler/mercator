d4222db50c9060e0ba08f9ec7b73938061d5fce2
Stephen Timcheck <stimcheck@cassini.seas.wustl.edu>
2019-11-14 20:53:46 -0600

    Fixed StatTest to use all sub-elements.  Command line input sizes also added.

--- examples/StatTest/StatDriver.cu
+++ examples/StatTest/StatDriver.cu
@@ -18,7 +18,7 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char** argv)
 {
   //const unsigned int NVALUES = 1000000000; // one BEEEELLION values
   //const unsigned int NVALUES = 4000; // one BEEEELLION values
@@ -54,18 +54,25 @@ int main()
   //const unsigned int MULTIPLIER = 1024;
   //const unsigned int MULTIPLIER = 256;
   
+
+
   ///////////////
   //EVEN TESTS 1B
   //const unsigned int NVALUES = 32000000;
   //const unsigned int NVALUES = 8000000;
   //const unsigned int NVALUES = 2000000;
-  const unsigned int NVALUES = 500000;
-  
+  //const unsigned int NVALUES = 500000;
+
+  //Half  
+  //const unsigned int NVALUES = 16000000;
+  //const unsigned int NVALUES = 4000000;
+  //const unsigned int NVALUES = 1000000;
+  //const unsigned int NVALUES = 250000;
+
   //const unsigned int MULTIPLIER = 32;
   //const unsigned int MULTIPLIER = 128;
   //const unsigned int MULTIPLIER = 512;
-  const unsigned int MULTIPLIER = 2048;
-  //const unsigned int MULTIPLIER = 1024;
+  //const unsigned int MULTIPLIER = 2048;
 
 
   ///////////////
@@ -74,7 +81,13 @@ int main()
   //const unsigned int NVALUES = 5333333;
   //const unsigned int NVALUES = 1333333;
   //const unsigned int NVALUES = 1000000;
-  
+
+  //Half  
+  //const unsigned int NVALUES = 8000000;
+  //const unsigned int NVALUES = 2666666;
+  //const unsigned int NVALUES = 666666;
+  //const unsigned int NVALUES = 500000;
+
   //const unsigned int MULTIPLIER = 64;
   //const unsigned int MULTIPLIER = 192;
   //const unsigned int MULTIPLIER = 768;
@@ -88,20 +101,29 @@ int main()
   //const unsigned int NVALUES = 999024;
   //const unsigned int NVALUES = 499756;
   
+  //Half
+  //const unsigned int NVALUES = 3968992;
+  //const unsigned int NVALUES = 998050;
+  //const unsigned int NVALUES = 499512;
+  //const unsigned int NVALUES = 249878;
+
   //const unsigned int MULTIPLIER = 129;
   //const unsigned int MULTIPLIER = 513;
   //const unsigned int MULTIPLIER = 1025;
   //const unsigned int MULTIPLIER = 2049;
 
 
+  const unsigned int NVALUES = atoi(argv[1]);
+  const unsigned int MULTIPLIER = atoi(argv[2]);
 
-  cout << "HERE1" << endl;
+  //cout << "HERE1" << endl;
   srand(0);
-  cout << "HERE2" << endl;
+  //cout << "HERE2" << endl;
 
+  /*
   unsigned int* d_compositeStorage;
   cudaMalloc(&d_compositeStorage, NVALUES * MULTIPLIER * sizeof(unsigned int));
-  cout << "AFTER MALLOC... ui " << sizeof(unsigned int) << " d " << sizeof(double) << " c " << sizeof(Composite) << endl;
+  ////cout << "AFTER MALLOC... ui " << sizeof(unsigned int) << " d " << sizeof(double) << " c " << sizeof(Composite) << endl;
 
   //double CPUverification[NVALUES];
   double* CPUverification = new double[NVALUES];
@@ -111,6 +133,8 @@ int main()
   Composite *inputValues = new Composite [NVALUES];
   unsigned int total = NVALUES;
   //Composite inputValues[NVALUES];
+  */
+
   /*
   for(unsigned int z = 0; z < NVALUES * MULTIPLIER; ++z) {
 	//h_compositeStorage[z] = z % MULTIPLIER;
@@ -146,12 +170,16 @@ int main()
   //unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
   //std::default_random_engine generator(seed);
 
+  unsigned int* h_compositeStorage = new unsigned int[NVALUES * MULTIPLIER];
+  unsigned int* d_compositeStorage;
+  cudaMalloc(&d_compositeStorage, NVALUES * MULTIPLIER * sizeof(unsigned int));
   for(unsigned int z = 0; z < NVALUES * MULTIPLIER; ++z) {
 	h_compositeStorage[z] = rand() % MULTIPLIER;
 	//h_compositeStorage[z] = z % MULTIPLIER;
 	//h_compositeStorage[z] = 1;
   }
 
+  /*
   for(unsigned int z = 0; z < NVALUES; ++z) {
 	unsigned int crand = rand() % MULTIPLIER;
 	inputValues[z] = Composite(d_compositeStorage + z * MULTIPLIER, crand);
@@ -162,15 +190,59 @@ int main()
 	}
 	//CPUverification[z] /= crand;
   }
+  */
+
+  unsigned int totalLenRemaining = NVALUES * MULTIPLIER;
+  vector<unsigned int> lineLengths = vector<unsigned int>();
+  while(totalLenRemaining != 0) {
+	unsigned int crand = rand() % MULTIPLIER;
+	if(crand > totalLenRemaining) {
+		lineLengths.push_back(totalLenRemaining);
+		totalLenRemaining = 0;
+	}
+	else {
+		lineLengths.push_back(crand);
+		totalLenRemaining -= crand;
+	}
+  }
+  unsigned int totalComposites = lineLengths.size();
+
+
+  //unsigned int* d_compositeStorage;
+  //cudaMalloc(&d_compositeStorage, NVALUES * MULTIPLIER * sizeof(unsigned int));
+  ////cout << "AFTER MALLOC... ui " << sizeof(unsigned int) << " d " << sizeof(double) << " c " << sizeof(Composite) << endl;
+
+  //double CPUverification[NVALUES];
+  double* CPUverification = new double[totalComposites];
+
+  //unsigned int h_compositeStorage[NVALUES * MULTIPLIER];
+  //unsigned int* h_compositeStorage = new unsigned int[NVALUES * MULTIPLIER];
+  Composite *inputValues = new Composite [totalComposites];
+  unsigned int total = NVALUES;
+
+
+  unsigned int prevLinePos = 0;
+  for(unsigned int z = 0; z < totalComposites; ++z) {
+	//unsigned int crand = rand() % MULTIPLIER;
+	inputValues[z] = Composite(d_compositeStorage + prevLinePos, lineLengths.at(z));
+	CPUverification[z] = 0;
+	//cout << distribution(generator) << endl;
+	for(unsigned int y = 0; y < lineLengths.at(z); ++y) {
+		CPUverification[z] += h_compositeStorage[prevLinePos + y];
+	}
+	prevLinePos += lineLengths.at(z);
+	//CPUverification[z] /= crand;
+  }
 
   //char c;
   //cin >> c;
 
-  cout << "BEFORE COPY..." << endl;
+  //cout << "BEFORE COPY..." << endl;
   cudaMemcpy(d_compositeStorage, h_compositeStorage, NVALUES * MULTIPLIER * sizeof(unsigned int), cudaMemcpyHostToDevice);
-  cout << "AFTER COPY..." << endl;
+  //cout << "AFTER COPY..." << endl;
   
-  double *outputValues = new double [NVALUES];
+  //double *outputValues = new double [NVALUES];
+  double *outputValues = new double [totalComposites];
   //cout << "HERE3" << endl;
   
   //unsigned int total = 0;
@@ -183,33 +255,36 @@ int main()
   
   // begin MERCATOR usage
   
-  cout << "BEFORE BUFFER ALLOCATION..." << endl;
-  Mercator::Buffer<Composite> inputBuffer(NVALUES);
+  //cout << "BEFORE BUFFER ALLOCATION..." << endl;
+  //Mercator::Buffer<Composite> inputBuffer(NVALUES);
+  Mercator::Buffer<Composite> inputBuffer(totalComposites);
   //Mercator::Buffer<unsigned int> outputBufferAccept(NVALUES);
-  Mercator::Buffer<double> outputBufferAccept(NVALUES);
+  //Mercator::Buffer<double> outputBufferAccept(NVALUES);
+  Mercator::Buffer<double> outputBufferAccept(totalComposites);
   //cout << "HERE5" << endl;
 
-  cout << "AFTER BUFFER ALLOCATION..." << endl;
+  //cout << "AFTER BUFFER ALLOCATION..." << endl;
   //int x;
   //cin >> x; 
   Stat efapp;
   
   //cout << "HERE6" << endl;
-  cout << "BEFORE SET SOURCE SINK..." << endl;
+  //cout << "BEFORE SET SOURCE SINK..." << endl;
   efapp.src.setSource(inputBuffer);
   efapp.snk.setSink(outputBufferAccept);
-  cout << "AFTER SET SOURCE SINK..." << endl;
+  //cout << "AFTER SET SOURCE SINK..." << endl;
   //cout << "HERE7" << endl;
   
   // move data into the input buffer
-  cout << "BEFORE SET INPUT BUFFER" << endl;
-  inputBuffer.set(inputValues, NVALUES);
-  cout << "AFTER SET INPUT BUFFER" << endl;
+  //cout << "BEFORE SET INPUT BUFFER" << endl;
+  //inputBuffer.set(inputValues, NVALUES);
+  inputBuffer.set(inputValues, totalComposites);
+  //cout << "AFTER SET INPUT BUFFER" << endl;
   //cout << "HERE8" << endl;
   
   cout << "RUNNING APP. . . " << endl;
-  unsigned int max = UINT_MAX * 32;
-  cout << UINT_MAX << "\t\t" << max << endl;
+  //unsigned int max = UINT_MAX * 32;
+  //cout << UINT_MAX << "\t\t" << max << endl;
   efapp.run();
   cout << "APP FINISHED. . ." << endl;
   
@@ -225,6 +300,8 @@ int main()
   //for(unsigned int j = 0; j < NVALUES*MULTIPLIER; j+=1)
 	//cout << "out[" << j << "]:\t" << outputValues[j] << endl;
 
+  //UNCOMMENT FOR VERIFICATION
+  /*
   std::vector<double> outs;
   std::vector<double> ins;
 
@@ -241,6 +318,7 @@ int main()
 		cout << "GPU[" << i << "] = " << outs[i] << "\t\tCPU[" << i << "] = " << ins[i] << "\t\tlength: " << inputValues[i].length << "\t\tdiff: " << outs[i] - ins[i] << endl;
 	}
   }
+  */
 
 
   /*
