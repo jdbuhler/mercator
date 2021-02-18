fdfa4991c6495c102019400394cdd091626d4f1d
Stephen Timcheck <stimcheck@lotus.engr.wustl.edu>
2021-02-17 22:33:34 -0600

    Fixed SynthGain output calculation, uses 5 stage pipeline now.

--- examples/SynthGain/SGDriver.cu
+++ examples/SynthGain/SGDriver.cu
@@ -1,36 +1,58 @@
 //
-// EFDRIVER.CU
-// Even-valued filter test
+// SGDRIVER.CU
+// Synthetic gains test, setting different gain for each node.
 //
 // MERCATOR
 // Copyright (C) 2018 Washington University in St. Louis; all rights reserved.
 //
 
 #include <iostream>
+#include <iomanip>
 
 #include "SynthGain.cuh"
+
+#define GAIN1 0.25
+#define GAIN2 11.5
+#define GAIN3 0.125
+#define GAIN4 0.25
+#define GAIN5 0.5
 
 using namespace std;
 
 int main()
 {
   //const unsigned int NVALUES = 1000000000; // one BEEEELLION values
-  const unsigned int NVALUES = 10000; // one BEEEELLION values
-  
-  unsigned int *outputValues = new unsigned int [NVALUES*16];
+  const unsigned int NVALUES = 100000000; // one BEEEELLION values
+  //const unsigned int NVALUES = 10000; // one BEEEELLION values
+  //const unsigned int NVALUES = 128; // one BEEEELLION values
   
   // begin MERCATOR usage
   
-  Mercator::Buffer<unsigned int> outputBufferAccept(NVALUES*16);
-  
   SynthGain app;
   
+  auto params1 = app.t1.getParams();
+  params1->avgGain = float(GAIN1);
+  auto params2 = app.t2.getParams();
+  params2->avgGain = float(GAIN2);
+  auto params3 = app.t3.getParams();
+  params3->avgGain = float(GAIN3);
+  auto params4 = app.t4.getParams();
+  params4->avgGain = float(GAIN4);
+  auto params5 = app.t5.getParams();
+  params5->avgGain = float(GAIN5);
+
+  float totalAvg = GAIN1 * GAIN2 * GAIN3 * GAIN4 * GAIN5;
+  unsigned int totalAvgInt = (unsigned int)(totalAvg);
+  if(totalAvg - totalAvgInt > 0.0) {
+	++totalAvgInt;
+  }
+
+  Mercator::Buffer<size_t> outputBufferAccept(NVALUES*totalAvgInt);
+  size_t *outputValues = new size_t [NVALUES*totalAvgInt];
+
   app.setNInputs(NVALUES);
   app.snk.setSink(outputBufferAccept);
 
-  auto params = app.t1.getParams();
-  params->avgGain = float(6.5);
-  
   app.run();
   
   // get data out of the output buffer
@@ -39,7 +61,10 @@ int main()
   
   // end MERCATOR usage
   
-  cout << "# outputs = " << outSize << endl;
+  cout << "Cumulative Gain = " << totalAvg << endl;
+  cout << "Total Output Space = " << NVALUES * totalAvgInt << endl;
+  cout << "Total Number Outputs = " << outSize << endl;
+  cout << "Expected Number Outputs = " << setprecision(20) << NVALUES * totalAvg << endl;
   /*
   for(unsigned int i = 0; i < outSize; i++)
     cout << outputValues[i] << endl;
--- examples/SynthGain/SynthGain.cu
+++ examples/SynthGain/SynthGain.cu
@@ -7,26 +7,30 @@
 //
 
 #include "SynthGain_dev.cuh"
-/*
+
+//Set the number of iterations of extra work for each input.
+#define ITERS 1000
+
+//Function for doing extra work on every output.
 __device__
-unsigned int munge(unsigned int key)
+size_t extra_work(size_t key)
 {
+  for(unsigned int i = 0; i < ITERS; ++i) {
   key = (key << 15) - key - 1;
   key = key ^ (key >> 12);
   key = key + (key << 2);
   key = key ^ (key >> 4);
   key = key * 2057;
   key = key ^ (key >> 16);
+  }
   return key;
 }
-*/
 
 __MDECL__
 void SynthGain_dev::
 type1<InputView>::run(const size_t& inputItem, unsigned int nInputs)
 {
   unsigned int tid = threadIdx.x;
-  //unsigned int v;
 
   auto params = getParams();
   float g = params->avgGain;
@@ -34,25 +38,16 @@ type1<InputView>::run(const size_t& inputItem, unsigned int nInputs)
   float partG = g - fullG;
 
   unsigned int totalOut = 0;
-  //int loopOut = fullG;
-  //if(partG > 0)
-    //loopOut += 1;
   
+  //Determine which threads should produce outputs.
   if (tid < nInputs) {
     totalOut = fullG;
+
+    //Determine which threads should produce an extra output, when there is a partial gain.
     if (partG > 0.0) {
-      totalOut += ((unsigned int)(nInputs * partG) <= tid ? 1 : 0);
+      totalOut += ((unsigned int)(nInputs * partG) <= tid ? 0 : 1);
     }
-    //totalOut += 2;
-
-    //v = munge((unsigned int) inputItem);
-
   }
-  
-  // If no channel is specified, push sends a value to the module's
-  // first output channel.
-  //push(v, tid < nInputs && (v % 2) == 0); // defaults to pushing to Out::accept
-  //for(unsigned int i = 0; i < loopOut; ++i)
 
   using R = Mercator::BlockReduce<unsigned int, THREADS_PER_BLOCK>;
   auto &bcast = Mercator::broadcast<unsigned int, THREADS_PER_BLOCK>;
@@ -60,8 +55,12 @@ type1<InputView>::run(const size_t& inputItem, unsigned int nInputs)
   unsigned int blockMaxHits = R::max(totalOut);
   blockMaxHits = bcast(blockMaxHits, 0);
 
+  //DEBUG
+  //printf("[%d, %d] g=%lf\tfullG=%d\tpartG=%lf\ttotalOut=%d\tblockMaxHits=%d\tnInputs*partG=%d\tpartTest=%d\n", blockIdx.x, tid, g, fullG, partG, totalOut, blockMaxHits, (unsigned int)(nInputs*partG), ((unsigned int)(nInputs * partG) <= tid ? 0 : 1));
+
+  //Output the total number of items for this thread.  Do extra work on the input before outputing.
   for(unsigned int i = 0; i < blockMaxHits; ++i)
-    push(1, i < totalOut); // defaults to pushing to Out::accept
+    push(extra_work(inputItem), i < totalOut); // defaults to pushing to Out::accept
 
 }
 
